6ad20bbf183929f96e28438af96cf41b393c40ab
KaKaShi-049 <beiblulcy@gmail.com>
2023-01-22 00:03:26 +0900

    次回公演 過去公演の更新と，それに付随するお知らせの更新， トップのスライドの画像を更新しました

--- docs/javascript/bg-switcher/keppeki-bg-switcher.js
+++ docs/javascript/bg-switcher/keppeki-bg-switcher.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
     $('.bg-slider').bgSwitcher({
-        images: ['./images/slide_show/slide1.jpg', './images/slide_show/slide2.jpg', './images/slide_show/slide3.jpg', './images/slide_show/slide4.jpg'], // 切替背景画像を指定
+        images: ['./images/slide_show/slide2201.jpg', './images/slide_show/slide2202.jpg', './images/slide_show/slide2203.jpg', './images/slide_show/slide2204.jpg', './images/slide_show/slide2205.jpg'], // 切替背景画像を指定
 	      interval: 10000, // 背景画像を切り替える間隔を指定 3000=3秒
         loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
         shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
